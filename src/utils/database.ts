bb628d432174ec9683c9826c4939c24d5e5494ce
Alexandre Bourdeaud'hui <alexandre.bourdeaudhui@gmail.com>
2021-07-27 21:16:57 +0200

    Little test

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,7 +1,7 @@
 /**
  * Package Import
  */
-import { Low, JSONFile } from 'lowdb';
+// import { Low, JSONFile } from 'lowdb';
 import path from 'path';
 import fs from 'fs';
 
@@ -16,15 +16,16 @@ type Data = {
  * Initialization Database instance
  */
 export default async () => {
-  const appDirectory = fs.realpathSync(process.cwd());
-  const file = path.join(appDirectory, 'db.json');
-  const adapter = new JSONFile<Data>(file);
-  const db = new Low<Data>(adapter)
+  // const appDirectory = fs.realpathSync(process.cwd());
+  // const file = path.join(appDirectory, 'db.json');
+  // const adapter = new JSONFile<Data>(file);
+  // const db = new Low<Data>(adapter);
 
   // Read data from JSON file, this will set db.data content
-  await db.read();
+  // await db.read();
 
   // Set default data
+  let db;
   db.data ||= { repositories: [] };
 
   return db;
